4bdfb8649b886746ec68ef23ae1ee98303e8704c
MohammadNasir520 <nasirahsan520@gmail.com>
2023-02-21 01:07:56 +0600

    paginated router done

--- routes/moviesRoutes.js
+++ routes/moviesRoutes.js
@@ -12,8 +12,21 @@ router.get('/get-all', getAllMovies);
 
 // get single movie by query parameter
 
-router.get('/get-single', getSinglesMovie)
+router.get('/get-single', getSinglesMovie);
 
+// get  paginated movies
+router.get('/get-paginated', async (req, res) => {
+    const page = Number(req.query.page) || 1;
+    const size = Number(req.query.size) || 2;
+    const skip = (page - 1) * size;
+
+    const movie = await movies.find().skip(skip).limit(size);
+    res.send({
+        page,
+        size,
+        movie
+    })
+})
 
 
 module.exports = router;
