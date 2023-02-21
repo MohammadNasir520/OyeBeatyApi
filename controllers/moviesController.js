c3d66631980820acae2c91bff5d783bb087b7cd5
MohammadNasir520 <nasirahsan520@gmail.com>
2023-02-22 00:16:40 +0600

    correction

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -48,7 +48,7 @@ exports.getSinglesMovie = async (req, res) => {
 }
 
 
-//  get paginated api
+//  get paginated api.
 exports.getPaginated = async (req, res) => {
     const page = Number(req.query.page) || 1;
     const size = Number(req.query.size) || 2;
